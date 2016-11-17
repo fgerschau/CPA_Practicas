a7db2c4f86cc13b4675882d026c10240aa508d39
Felix <felix.gerschau@gmail.com>
2016-11-17 18:29:08 +0100

    First commit

---
+++ material/pencaja1.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#PBS -l nodes=1,walltime=00:05:00	
+#PBS -q cpa
+#PBS -d .	
+OMP_NUM_THREADS=1 ./pencaja1
+OMP_NUM_THREADS=2 ./pencaja1
+OMP_NUM_THREADS=4 ./pencaja1
+OMP_NUM_THREADS=8 ./pencaja1
+OMP_NUM_THREADS=16 ./pencaja1
+OMP_NUM_THREADS=32 ./pencaja1
